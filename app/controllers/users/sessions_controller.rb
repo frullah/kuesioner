7b66c77885486033b5ebd6d88accdfd3b4891067
Fajarullah <frullah12@gmail.com>
2021-01-01 19:45:46 +0700

    remove notice flash after login or logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,14 +11,16 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    flash.delete(:notice)
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+    flash.delete(:notice)
+  end
 
   # protected
 
