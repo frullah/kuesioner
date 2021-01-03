8f3eb1c9a4cb8b456c3797c545f82dee0dd4a943
Fajarullah <frullah12@gmail.com>
2021-01-03 17:24:45 +0700

    remove unnecessary comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
 
   belongs_to :authenticatable, polymorphic: true
 
-  # validates :password, presence: true, length: {maximum: 48, minimum: 8}, on: :create
-  # validates :password, presence: true, length: {maximum: 48, minimum: 8}, on: :update, if: :password_changed?
-
   def email_required?
     false
   end
