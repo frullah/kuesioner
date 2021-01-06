87d36de48831cc6d6d793c5d1a5bfdfebd6b3734
Fajarullah <frullah12@gmail.com>
2021-01-06 11:26:52 +0700

    remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,8 @@
-import $ from 'jquery'
 import 'admin-lte/build/js/Layout'
 import 'admin-lte/build/js/PushMenu'
-import BSN from 'bootstrap.native'
+import 'bootstrap.native'
 
 require('@rails/ujs').start()
-// require('turbolinks').start()
-
-document.addEventListener('turbolinks:load', () => {
-  $(document.body).Layout('fixLayoutHeight')
-
-  // fix adminLTE sidebar state
-  window.dispatchEvent(new Event('resize'))
-
-  BSN.initCallback()
-})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
